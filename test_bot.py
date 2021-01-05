927ac8ca9278e079ae741dedae2f45c950b4096d
Nikelui <nikelui@hotmail.it>
2021-01-05 15:46:28 +0100

    Implemented dado semaforo and help

--- test_bot.py
+++ test_bot.py
@@ -1,8 +1,20 @@
 from discord.ext import commands
+import discord
 import random
 import d20
 
-bot = commands.Bot(command_prefix='!')
+
+class MyHelpCommand(commands.DefaultHelpCommand):    
+    def get_ending_note(self):
+        return 'Scrivi !help <comando> per avere più informazioni su uno specifico comando.'
+    def command_not_found(self, command):
+        return f'Comando <{command}> non trovato'
+
+
+help_command = MyHelpCommand(brief='-> Mostra questo messaggio',
+    no_category = 'Comandi:', commands_heading = 'Comandi:')
+
+bot = commands.Bot(command_prefix='!', help_command=help_command)
 
 
 @bot.event
@@ -10,25 +22,22 @@ async def on_ready():
     print('We have logged on as {0.user}'.format(bot))
 
 
-@bot.command(name='help')
-async def _help(ctx, arg=""):
-    if arg == "":
-        embed = discord.Embed(
-            title="Help",
-            description="Help description",
-            colour=discord.Colour.blurple()
-        )
-        embed.add_field(name="u.join", value="Join a game",inline=True)
-        embed.add_field(name="u.start", value="Start a game", inline=True)
-        await ctx.channel.send(embed=embed)
-
-
 # New implementation using d20
-@bot.command(name='roll')
+@bot.command(name='roll', brief='-> Tira un dado')
 async def _roll(ctx, dice: str):
+    """Tira un dado usando un'espressione del tipo NdX:
+- N: numero di dadi [int]
+- X: tipo di dado [int o 's']
+     l'espressione Nds viene usata per tirare N dadi semaforo.
+
+Restituisce: singoli valori e somma dei tiri
+
+Per maggiori informazioni sulla sintassi utilizzata:
+https://d20.readthedocs.io/en/latest/start.html#dice-syntax
+"""
     try:
         res = d20.roll(dice)
-        await ctx.send('Result: {}'.format(str(res)))
+        await ctx.send('Risultato: {}'.format(str(res)))
     except d20.errors.RollSyntaxError:
         if 'ds' in dice:  # dado semaforo
             green, yellow, red = 0, 0, 0  # to store results
@@ -44,33 +53,38 @@ async def _roll(ctx, dice: str):
                     else:
                         red += 1
                 result_dice = '  '.join(['🟩']*green + ['🟧']*yellow + ['🟥']*red)
-                result_string = ', '.join(['**Green** = {}'.format(green)]*(green>0) + 
-                                         ['**Yellow** = {}'.format(yellow)]*(yellow>0) +
-                                         ['**Red** = {}'.format(red)]*(red>0))
-                if num > 15:
-                    await ctx.send('Result: {}'.format(result_string))
+                result_string = ', '.join(['**Verde** = {}'.format(green)]*(green>0) + 
+                                          ['**Giallo** = {}'.format(yellow)]*(yellow>0) +
+                                          ['**Rosso** = {}'.format(red)]*(red>0))
+                if num > 10:
+                    await ctx.send('Risultato: {}'.format(result_string))
                 else:
-                    await ctx.send('Result: {}\n{}'.format(result_dice, result_string))
+                    await ctx.send('Risultato: {}\n{}'.format(result_dice, result_string))
                 
             except ValueError:
-                await ctx.send('Enter a valid dice expression (try !help)')
+                await ctx.send('Inserire un\'espressione valida (prova !help)')
                 return
         else:
-            await ctx.send('Enter a valid dice expression (try !help)')
+            await ctx.send('Inserire un\'espressione valida (prova !help)')
             return
 
 
-@bot.command(name='quit')
+@bot.command(name='quit', brief='-> Disconnetti il bot')
 async def _quit(ctx):
-    with open('goodbyes.txt', 'r') as quotes:
+    """Disconnette il bot dal server e lascia una citazione. Punti extra se si indovina la fonte."""
+    with open('quote.txt', 'r') as quotes:
         lines = quotes.readlines()
         await ctx.send('{}'.format(lines.pop(random.randint(0, len(lines)-1))))
     await bot.logout()
 
 
-@bot.command(name='deal')
-async def _deal(ctx):
+@bot.command(name='deal', brief='-> Assegna carte')
+async def _deal(ctx, N: int):
+    """Pesca N carte da un mazzo preesistente nel canale e le assegna all'autore del comando.
+WORK IN PROGRESS
+"""
     await ctx.send('Just a test to see if the command works')
+
 
 with open('token.txt', 'r') as tk:
     token = tk.read()
